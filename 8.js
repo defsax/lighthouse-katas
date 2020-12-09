d474e46bc5ec8ca3f8688048753619c56d3b9870
Perry Defayette <defayetteperry@gmail.com>
2020-12-09 00:11:56 +0000

    Completed repeating numbers kata.

---
+++ 8.js
@@ -0,0 +1,15 @@
+const repeatNumbers = function(data){
+  let value = '';
+  for(let i = 0; i < data.length; i++){
+    for(let j = 0; j < data[i][1]; j++){
+      value += data[i][0];
+    }
+    if(i < data.length - 1)
+      value += ', ';
+  }
+  return value;
+};
+
+console.log(repeatNumbers([[1, 10]]));
+console.log(repeatNumbers([[1, 2], [2, 3]]));
+console.log(repeatNumbers([[10, 4], [34, 6], [92, 2]]));
