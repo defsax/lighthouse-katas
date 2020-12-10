56bbca10d295f37771cb6816f1174fdb05b33b22
Perry Defayette <defayetteperry@gmail.com>
2020-12-10 20:40:40 +0000

    Finished bakeoff kata. Involved looping through multiple arrays and matching data.

--- 12.js
+++ 12.js
@@ -1,4 +1,39 @@
+const ingredientCheck = function(bakery, ingredient){
+  let inStock = false;
+  //Loop through each ingredient in bakery
+  for(let i of bakery){
+    //If there's a match, bakery has ingredient in stock
+    if(i === ingredient){
+      inStock = true;
+    }
+  }
+  return inStock;
+};
+
 const chooseRecipe = function(bakeryA, bakeryB, recipes) {
+  let bakeryAHasIngredient = false;
+  let bakeryBHasIngredient = false;
+  let chosenRecipe = '';
+
+  //Loop through recipes
+  for(let i of recipes){
+    //Loop through each ingredient in each recipe
+    for(let j of i.ingredients){
+      //Check if bakeries contain current ingredient. If they do, set bool to true.
+      if(ingredientCheck(bakeryA, j))
+        bakeryAHasIngredient = true;
+      if(ingredientCheck(bakeryB, j))
+        bakeryBHasIngredient = true;
+
+      //If both bakeries have an ingredient from the current recipe, this recipe is the winner!
+      if(bakeryAHasIngredient && bakeryBHasIngredient)
+        chosenRecipe = i.name;
+    }
+    //If we get through current recipe's ingredients and there is no match, reset bools.
+    bakeryAHasIngredient = false;
+    bakeryBHasIngredient = false;
+  }
+  return chosenRecipe;
 };
 
 let bakeryA = ['saffron', 'eggs', 'tomato paste', 'coconut', 'custard'];
