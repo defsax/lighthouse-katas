f12f318cd5d6412ce76a91af6ce112413fdba596
Per <defayetteperry@gmail.com>
2020-12-16 15:35:41 -0500

    Finished square code kata.

--- 18.js
+++ 18.js
@@ -1,14 +1,36 @@
 const squareCode = function(message) {
-  let coded = '';
+  let coded = [];
+  let rows = [];
+  let columns = [];
   
   //remove spaces
   message = message.split(' ').join('');
 
-  
+  //calculate length and height
+  let length = Math.ceil(Math.sqrt(message.length));
+  let height = Math.ceil(message.length / length);
 
-  for(let i of message){
-    console.log(i);
+  //loop from 0 to l * h so that last row gets added
+  for(let i = 0; i <= length * height; i++){
+    if(i % length === 0 && i !== 0){
+      rows.push(columns);
+      columns = [];
+    }
+    //don't add letters that aren't there
+    if(message[i] !== undefined)
+      columns.push(message[i]);
   }
+
+  //loop back over array and add mixed letters to encoded return array
+  for(let i = 0; i < length; i++){
+    for(let j = 0; j < height; j++){
+      if(rows[j][i] !== undefined){
+        coded += rows[j][i];
+      }
+    }
+    coded += ' ';
+  }
+
   return coded;
 };
 
